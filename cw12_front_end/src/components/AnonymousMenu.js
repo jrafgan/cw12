77c1b4b596bf95049fac7ace5b22d2d944cd8fd5
Maksat Duishenaliev <maksatovy@gmail.com>
2019-05-25 17:31:20 +0600

    login and register containers added

--- cw12_front_end/src/components/AnonymousMenu.js
+++ cw12_front_end/src/components/AnonymousMenu.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import FacebookLogin from "./FacebookLogin";
+import {NavLink} from "react-router-dom";
 
 const AnonymousMenu = () => (
     <div className="anonymous_menu">
         <div>
+            <NavLink to="/register" exact>Register</NavLink>
+        </div>
+        <div>
             <FacebookLogin/>
+        </div>
+        <div>
+            <NavLink to="/login" exact>Login</NavLink>
         </div>
     </div>
 );
