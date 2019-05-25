80e2b59ff82a1425cd0137747d45a1e3b97409a8
Maksat Duishenaliev <maksatovy@gmail.com>
2019-05-25 17:51:45 +0600

    state added to for pop up div

--- cw12_front_end/src/containers/Main.js
+++ cw12_front_end/src/containers/Main.js
@@ -1,30 +1,46 @@
 import React, {Component, Fragment} from 'react';
 import '../App.css'
-import {deleteCocktail, getCocktails, toggleCocktailPublish} from "../store/actions/photoActions";
+import {deletePhoto, getPhoto, getPhotos} from "../store/actions/photoActions";
 import connect from "react-redux/es/connect/connect";
 import ImageThumbnail from "../components/ImageThumbnail";
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 
 
 class Main extends Component {
 
+    state = {
+      popUpShow: false
+    };
+
     componentDidMount() {
-        this.props.getCocktails();
+        this.props.getPhotos();
     }
 
     componentDidUpdate(prevProps) {
         if (this.props.user !== prevProps.user) {
-            this.props.getCocktails();
+            this.props.getPhotos();
         }
     }
 
-    deleteCocktail = e => {
-        this.props.deleteCocktail(e.target.id);
+    photoInfo = e =>{
+        const id = e.target.id;
+        this.props.getPhoto(id);
+        this.setState({popUpShow: true})
     };
 
-    togglePublishCocktail = e => {
-        this.props.togglePublishCocktail(e.target.id);
+    userPhotos = e =>{
+        const id = e.target.id;
+        this.props.getPhoto(id);
     };
+
+    closePopUp = e =>{
+        this.setState({popUpShow: false})
+    };
+
+    deletePhoto = e => {
+        this.props.deletePhoto(e.target.id);
+    };
+
 
     render() {
 
@@ -32,36 +48,40 @@ class Main extends Component {
             <div className="App">
                 <div className="list_div">
                     <div className="column">
-                        <p className="cocktail_p">Cocktails</p>
-                        {this.props.cocktails ? this.props.cocktails.map(item => {
-                            return <div className="cocktail_thumbnail" key={item._id} id={item._id}>
+                        <p className="photo_p">Cocktails</p>
+                        {this.props.photos ? this.props.photos.map(item => {
+                            return <div className="photo_thumbnail" key={item._id} id={item._id}>
                                 <ImageThumbnail image={item.image} class="img_thumbnail"/>
-                                <Link to={"/cocktail_info/" + item._id}>{item.name}</Link>
+                                <span id={item._id} onClick={this.photoInfo} className="photo_title">{item.title}</span>
+                                <span>by : </span><NavLink to={"/user_photos/" + item.user._id} exact onClick={this.userPhotos}>{item.user.name}</NavLink>
                                 <div>
-                                {this.props.user && this.props.user.role === 'admin' ? <Fragment>{!item.published ? <button id={item._id} className="publish_btn"
-                                                                                                         onClick={this.togglePublishCocktail}>Publish</button> : <button id={item._id} className="unpublish_btn"
-                                                                                                                                                                         onClick={this.togglePublishCocktail}>Unpublish</button>}
+                                {this.props.user && this.props.user._id === item.user._id ?
                                     <button id={item._id} className="delete_btn"
-                                            onClick={this.deleteCocktail}>Delete</button></Fragment> : <Fragment>{!item.published ? <p className="unpublish_btn">On moderation</p> : null}</Fragment>}
+                                            onClick={this.deletePhoto}>Delete</button> : null}
                                 </div>
                             </div>
                         }) : null}
                     </div>
                 </div>
+                {this.state.popUpShow && this.props.photo ? <div className="pop_up" onClick={this.closePopUp}>
+                    {<div className="big_img_div"><ImageThumbnail image={this.props.photo.image} class="big_img_thumbnail"/>
+                        <p className="photo_p border" onClick={this.closePopUp}>Close</p></div>}
+                </div> : null}
             </div>
         );
     }
 }
 
 const mapStateToProps = state => ({
-    cocktails: state.cocktail.cocktails,
+    photos: state.photo.photos,
+    photo: state.photo.photo,
     user: state.users.user,
 });
 
 const mapDispatchToProps = dispatch => ({
-    getCocktails: () => dispatch(getCocktails()),
-    deleteCocktail: id => dispatch(deleteCocktail(id)),
-    togglePublishCocktail: id => dispatch(toggleCocktailPublish(id)),
+    getPhotos: () => dispatch(getPhotos()),
+    getPhoto: id => dispatch(getPhoto(id)),
+    deletePhoto: id => dispatch(deletePhoto(id)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Main);
