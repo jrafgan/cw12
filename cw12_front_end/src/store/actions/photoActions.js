dcd7d9b1113b881f06ad140f7e64c7ec2630db3e
Maksat Duishenaliev <maksatovy@gmail.com>
2019-05-25 17:54:30 +0600

    some action creator renamed

--- cw12_front_end/src/store/actions/photoActions.js
+++ cw12_front_end/src/store/actions/photoActions.js
@@ -1,99 +1,42 @@
 import axios from '../../axios-api';
 import {push} from "connected-react-router";
 
-export const FETCH_COCKTAILS_SUCCESS = 'FETCH_COCKTAILS_SUCCESS';
-export const FETCH_ARTIST_SUCCESS = 'FETCH_ARTIST_SUCCESS';
-export const FETCH_ALBUMS_SUCCESS = 'FETCH_ALBUMS_SUCCESS';
-export const FETCH_COCKTAIL_SUCCESS = 'FETCH_COCKTAIL_SUCCESS';
-export const FETCH_TRACKS_SUCCESS = 'FETCH_TRACKS_SUCCESS';
-export const FETCH_TRACKSBYARTIST_SUCCESS = 'FETCH_TRACKSBYARTIST_SUCCESS';
-export const FETCH_TRACKSBYALBUM_SUCCESS = 'FETCH_TRACKSBYALBUM_SUCCESS';
+export const FETCH_PHOTOS_SUCCESS = 'FETCH_PHOTOS_SUCCESS';
+export const FETCH_PHOTO_SUCCESS = 'FETCH_PHOTO_SUCCESS';
 export const FETCH_FAILURE = 'FETCH_FAILURE';
 
-export const fetchCocktailsSuccess = cocktails => ({type: FETCH_COCKTAILS_SUCCESS, cocktails});
-export const fetchArtistSuccess = cocktail => ({type: FETCH_ARTIST_SUCCESS, artist: cocktail});
-export const fetchAlbumsSuccess = albums => ({type: FETCH_ALBUMS_SUCCESS, albums});
-export const fetchCocktailSuccess = cocktail => ({type: FETCH_COCKTAIL_SUCCESS, cocktail});
-export const fetchTracksSuccess = tracks => ({type: FETCH_TRACKS_SUCCESS, tracks});
-export const fetchTracksByArtistSuccess = tracks => ({type: FETCH_TRACKSBYARTIST_SUCCESS, tracks});
-export const fetchTracksByAlbumSuccess = tracks => ({type: FETCH_TRACKSBYALBUM_SUCCESS, tracks});
+export const fetchPhotosSuccess = photos => ({type: FETCH_PHOTOS_SUCCESS, photos});
+export const fetchPhotoSuccess = photo => ({type: FETCH_PHOTO_SUCCESS, photo});
 export const fetchFailure = error => ({type: FETCH_FAILURE, error});
 
-export const getCocktails = id => {
+export const getPhotos = id => {
     return dispatch => {
-        let path = '/cocktails';
+        let path = '/photos';
 
         if (id) {
             path += '?user=' + id;
         }
         return axios.get(path).then(
             response => {
-                dispatch(fetchCocktailsSuccess(response.data));
+                dispatch(fetchPhotosSuccess(response.data));
+                console.log('thi is het Photos API res', response.data)
             });
     }
 };
-export const getAlbums = artistId => {
-    return dispatch => {
-        let path = '/albums';
 
-        if (artistId) {
-            path += '?cocktail=' + artistId;
-        }
-        return axios.get(path).then(
-            response => {
-                dispatch(fetchAlbumsSuccess(response.data));
-            });
-    };
-};
-
-export const getCocktail = id => {
+export const getPhoto = id => {
     return dispatch => {
-        return axios.get('/cocktails/' + id).then(
+        return axios.get('/photos/' + id).then(
             response => {
-                dispatch(fetchCocktailSuccess(response.data));
+                dispatch(fetchPhotoSuccess(response.data));
                 console.log(response.data);
             });
     };
 };
 
-export const getTracks = () => {
+export const addPhoto = cocktailData => {
     return dispatch => {
-        return axios.get('/tracks').then(
-            response => {
-                dispatch(fetchTracksSuccess(response.data));
-            });
-    };
-};
-export const getTracksByAlbum = albumId => {
-    return dispatch => {
-        return axios.get('/tracks?cocktail=' + albumId).then(
-            response => {
-                dispatch(fetchTracksSuccess(response.data));
-                console.log(response.data);
-            });
-    };
-};
-
-export const createArtist = artistData => {
-    return dispatch => {
-        return axios.post('/cocktails', artistData).then(
-            response => {
-                console.log(response.data);
-                dispatch(push('/'))
-            },
-            error => {
-                if (error.response) {
-                    dispatch(fetchFailure(error.response.data));
-                } else {
-                    dispatch(fetchFailure({global: "No network connection "}))
-                }
-            });
-    };
-};
-
-export const createCocktail = cocktailData => {
-    return dispatch => {
-        return axios.post('/cocktails', cocktailData).then(
+        return axios.post('/photos', cocktailData).then(
             response => {
                 dispatch(push('/'));
                 console.log(response.data);
@@ -108,21 +51,13 @@ export const createCocktail = cocktailData => {
     };
 };
 
-export const deleteCocktail = id => {
+export const deletePhoto = id => {
     return dispatch => {
-        return axios.delete('/cocktails?id=' + id).then(
+        return axios.delete('/photos?id=' + id).then(
             response => {
-                dispatch(fetchCocktailsSuccess(response.data));
+                dispatch(fetchPhotosSuccess(response.data));
             });
     };
 };
 
-export const toggleCocktailPublish = id => {
-    return dispatch => {
-        return axios.post('/cocktails/' + id + '/toggle_published').then(
-            response => {
-                dispatch(fetchCocktailsSuccess(response.data));
-            });
-    };
-};
 
